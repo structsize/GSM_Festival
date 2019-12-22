757ab82c85b6890bc6262eb495740afbbabb38cf
structsize <52183019+structsize@users.noreply.github.com>
2019-12-23 08:56:59 +0900

    [DELETE] UI
    this ui is no used in application

--- GSM_Festival/app/src/main/java/com/example/gsm_festival/KcalCheck.kt
+++ GSM_Festival/app/src/main/java/com/example/gsm_festival/KcalCheck.kt
@@ -13,15 +13,6 @@ class KcalCheck : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kcal_check)
-//        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-//
-//        val navController = findNavController(R.id.nav_host_fragment)
-//        // Passing each menu ID as a set of Ids because each
-//        // menu should be considered as top level destinations.
-//        val appBarConfiguration = AppBarConfiguration(setOf(
-//                R.id.meal, R.id.navigation_dashboard, R.id.navigation_notifications))
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-//        navView.setupWithNavController(navController)
 
     }
 }
